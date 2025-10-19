4b060820ad2cf45f8979e7ccf6c41196a213b4f9
Арсений <73458752+she1kopr9d@users.noreply.github.com>
2025-10-19 08:38:07 +0300

    Add unsafe CRUD

--- app/main.py
+++ app/main.py
@@ -4,9 +4,16 @@ import utils.linked_routers
 
 app = fastapi.FastAPI()
 
+origins = [
+    "http://158.160.201.116",
+]
+
 app.add_middleware(
     fastapi.middleware.cors.CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 utils.linked_routers.load_all_service_routers()
